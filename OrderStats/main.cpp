20940a937ae16c610bb64590bc1480716cd0cc38
twitecki <twitecki@pandora02.cs.trinity.edu>
2015-02-01 23:20:22 -0600

    Fully working in test code but not srvr

--- OrderStats/main.cpp
+++ OrderStats/main.cpp
@@ -44,10 +44,15 @@ void startTests() {
     end = clock();
     cout << "Execution Time: " <<  double(end - begin) / CLOCKS_PER_SEC << " seconds" <<endl;
   cout << "==================================================" << "\n";
-
   cout << "==================================================" << "\n";
     begin = clock();
   test3();
+    end = clock();
+    cout << "Execution Time: " <<  double(end - begin) / CLOCKS_PER_SEC << " seconds" <<endl;
+  cout << "==================================================" << "\n";
+  cout << "==================================================" << "\n";
+    begin = clock();
+  test4();
     end = clock();
     cout << "Execution Time: " <<  double(end - begin) / CLOCKS_PER_SEC << " seconds" <<endl;
   cout << "==================================================" << "\n";
@@ -90,25 +95,27 @@ void test1() {
   cout << "========================================================" << "\n";
   cout << "==================== SMALL INT TEST ====================" << "\n";
   cout << "========================================================" << "\n";
-  // vector<int> v{3, 5, 4, 0, 1, 2};
-   vector<int> v{10,5,24,47,3,7,19,20,201,45,63,41}; 
-   vector<int> v1{10,5,24,47,3,7,19,20,201,45,63,41}; 
-  printVector(v);
-  printVector(v1);
   int index = 4; 
+  vector<int> v{10,5,24,47,3,7,19,20,201,45,63,41}; 
+  printVector(v);
   auto orderAns = randomOrderStat(v.begin(),v.end(),less, index);
+  vector<int> v1{10,5,24,47,3,7,19,20,201,45,63,41}; 
+  printVector(v1);
   auto selectAns = linearOrderStat(v1.begin(),v1.end(),less, index);
+  cout << "Vectors After Stat\n";
   printVector(v);
   printVector(v1);
   sort(v.begin(), v.end());
   sort(v1.begin(), v1.end());
-  cout << "Sorted Vector: ";
+  cout << "Sorted Vectors: ";
   printVector(v);
+  printVector(v1);
   int realAns = v[index];
+  int realAns1 = v1[index];
   if (*orderAns == realAns) cout << "Returned Correct Element For Ordered Stat" << endl;
   else cout << "Returned INCORRECT Element For Ordered Stat" << "   Returned: " << *orderAns << "    Should be: " << realAns << endl;
-  if (*selectAns == realAns) cout << "Returned Correct Element For Linear Stat" << endl;
-  else cout << "Returned INCORRECT Element For Linear Stat" << "   Returned: " << *selectAns << "    Should be: " << realAns << endl;
+  if (*selectAns == realAns1) cout << "Returned Correct Element For Linear Stat" << endl;
+  else cout << "Returned INCORRECT Element For Linear Stat" << "   Returned: " << *selectAns << "    Should be: " << realAns1 << endl;
   cout << "========================================================" << "\n";
   cout << endl;
 }
@@ -127,16 +134,22 @@ void test2() {
     v.push_back(rand() % 100);
     v1.push_back(rand() % 100);
   }
-  int index = 18; 
+  int index = 8; 
   auto orderAns = randomOrderStat(v.begin(),v.end(),less, index);
   auto selectAns = linearOrderStat(v1.begin(),v1.end(),less, index);
+  printVector(v);
+  cout << "\n";
+  printVector(v1);
+  cout << "\n";
   sort(v.begin(), v.end());
   sort(v1.begin(), v1.end());
+  printVector(v);
+  printVector(v1);
   int realAns = v[index];
   int realAns1 = v1[index];
-  if (*orderAns == realAns) cout << "Returned Correct Element For Ordered Stat" << endl;
+  if (*orderAns == realAns) cout << "Returned Correct Element For Ordered Stat: " << *orderAns << endl;
   else cout << "Returned INCORRECT Element For Ordered Stat" << "   Returned: " << *orderAns << "    Should be: " << realAns << endl;
-  if (*selectAns == realAns1) cout << "Returned Correct Element For Linear Stat" << endl;
+  if (*selectAns == realAns1) cout << "Returned Correct Element For Linear Stat: " << *selectAns << endl;
   else cout << "Returned INCORRECT Element For Linear Stat" << "   Returned: " << *selectAns << "    Should be: " << realAns1 << endl;
   cout << endl;
 }
@@ -144,22 +157,48 @@ void test2() {
 void test3() {
   cout << endl;
   cout << "======================================================" << "\n";
+  cout << "================== SMALL STRING TEST =================" << "\n";
+  cout << "======================================================" << "\n";
+  srand(1);
+  vector<string> v{"c","b","a","d","z","t","w"};
+  vector<string> v1{"c","b","a","d","z","t","w"};
+  int index = 5; 
+  auto orderAns = randomOrderStat(v.begin(),v.end(), [](string x, string y) { return (x<y);}, index);
+  auto selectAns = linearOrderStat(v.begin(),v.end(), [](string x, string y) { return (x<y);}, index);
+  sort(v.begin(), v.end());
+  sort(v1.begin(), v1.end());
+  string realAns = v[index];
+  string realAns1 = v1[index];
+  if (*orderAns == realAns) cout << "Returned Correct Element For Ordered Stat" << endl;
+  else cout << "Returned INCORRECT Element For Ordered Stat" << "   Returned: " << *orderAns << "    Should be: " << realAns << endl;
+  if (*selectAns == realAns1) cout << "Returned Correct Element For Linear Stat" << endl;
+  else cout << "Returned INCORRECT Element For Linear Stat" << "   Returned: " << *selectAns << "    Should be: " << realAns1 << endl;
+  cout << endl;
+}
+//Big Int Test
+void test4() {
+  cout << endl;
+  cout << "======================================================" << "\n";
   cout << "==================== BIG INT TEST ===================" << "\n";
   cout << "======================================================" << "\n";
-  //srand(25);
-  srand(1);
+  srand(25);
+  //srand(1);
   vector<int> v;
+  vector<int> v2;
   for (int i = 0; i < 10000000; ++i) {
-    v.push_back(rand() % 500000);
+    v.push_back(rand() % 5000000);
+    v2.push_back(rand() % 5000000);
   }
   int index = 100; 
   auto orderAns = randomOrderStat(v.begin(),v.end(),less, index);
-  auto selectAns = linearOrderStat(v.begin(),v.end(),less, index);
+  auto selectAns = linearOrderStat(v2.begin(),v2.end(),less, index);
   sort(v.begin(), v.end());
+  sort(v2.begin(), v2.end());
   int realAns = v[index];
+  int realAns2 = v2[index];
   if (*orderAns == realAns) cout << "Returned Correct Element For Ordered Stat" << endl;
   else cout << "Returned INCORRECT Element For Ordered Stat" << "   Returned: " << *orderAns << "    Should be: " << realAns << endl;
-  if (*selectAns == realAns) cout << "Returned Correct Element For Linear Stat" << endl;
-  else cout << "Returned INCORRECT Element For Linear Stat" << "   Returned: " << *selectAns << "    Should be: " << realAns << endl;
+  if (*selectAns == realAns2) cout << "Returned Correct Element For Linear Stat" << endl;
+  else cout << "Returned INCORRECT Element For Linear Stat" << "   Returned: " << *selectAns << "    Should be: " << realAns2 << endl;
   cout << endl;
 }
--- OrderStats/order.h
+++ OrderStats/order.h
@@ -1,6 +1,7 @@
 #ifndef ORDER
 #define ORDER
 
+#include <iostream>
 #include <vector>
 
 using std::cout;
@@ -15,7 +16,8 @@ T linearOrderStat(T begin, T end,C lt,S index);
 template<typename T, typename C> T randomizedPartition(T begin, T end, const C &lt) {
   auto i = -1;
   auto randomInd = rand() % (end-begin);
-  iter_swap(begin+randomInd, end);
+  //cout << "RAND: " << randomInd << "\n";
+  iter_swap(begin+randomInd, end-1);
   auto x = *(end - 1);
   //may want to make const ref to end - 1
   for (auto j = begin; j != (end - 1); ++j) {
@@ -47,25 +49,25 @@ T randomOrderStat(T begin, T end,C lt,S index) {
     return begin;
   } else {
     auto pivot = randomizedPartition(begin,end,lt);
-                                                          // std::cout << "=================================================\n";  
-                                                          // std::cout << "Pivot: " << *pivot << '\n'; 
-                                                          // std::cout << "Vector: ";
-                                                          // for(auto i = begin; i != end; ++i) {
-                                                          //   std::cout << *i << " ";
-                                                          // }
-                                                          // std::cout << '\n';
-    auto subSize = pivot - begin;
-                                                          // std::cout << "Index = " << index << " and subSize = " << subSize << "     ";
-    if(index == subSize) {
-                                                          // std::cout << "Returned Element " << *pivot << '\n';
+                                                           //std::cout << "=================================================\n";  
+                                                           //std::cout << "Pivot: " << *pivot << '\n'; 
+                                                           //std::cout << "Vector: ";
+                                                           //for(auto i = begin; i != end; ++i) {
+                                                             //std::cout << *i << " ";
+                                                           //}
+                                                           //std::cout << '\n';
+    auto subSize = pivot - begin + 1;
+                                                           //std::cout << "Index = " << index << " and subSize = " << subSize << "     ";
+    if(index == subSize - 1) {
+                                                           //std::cout << "Returned Element " << *pivot << '\n';
       return pivot;
     } else if(index < subSize) {
                                                           // std::cout << "Moved Left\n";
-      return randomOrderStat(begin, pivot-1, lt, index);
+      return randomOrderStat(begin, pivot, lt, index);
     } else {
                                                           // std::cout << "Moved Right\n";
 
-      return randomOrderStat(pivot+1, end, lt, index-(subSize+1));
+      return randomOrderStat(pivot+1, end, lt, index-(subSize));
     }
   }
 }
@@ -102,11 +104,11 @@ template<typename T, typename C> typename T::value_type medianOfMedians(T begin,
   cout << "======================================================\n";
 */
   if ((end - begin) <= 5) {
-      //cout << "Returning: " << *(((end - begin) / 2) + begin) << "\n";
       return *(((end - begin) / 2) + begin);
   }
   int numSubs = (end - begin) / 5;
-  std::vector<int> medians;
+  //std::vector<int> medians;
+  std::vector<typename T::value_type> medians;
   if((end - begin) % 5 != 0) ++numSubs;
   for(int i = 0; i < numSubs; ++i) {
 	 int startingIndex = (5 * i);
@@ -150,18 +152,18 @@ T linearOrderStat(T begin, T end,C lt,S index) {
                                                              //std::cout << *i << " ";
                                                            //}
                                                            //std::cout << '\n';
-    auto subSize = pivot - begin;
+    auto subSize = pivot - begin + 1;
                                                            //std::cout << "Index = " << index << " and subSize = " << subSize << "     ";
-    if(index == subSize) {
+    if(index == subSize - 1) {
                                                            //std::cout << "Returned Element " << *pivot << '\n';
       return pivot;
     } else if(index < subSize) {
                                                            //std::cout << "Moved Left\n";
-      return linearOrderStat(begin, pivot-1, lt, index);
+      return linearOrderStat(begin, pivot, lt, index);
     } else {
                                                            //std::cout << "Moved Right\n";
 
-      return linearOrderStat(pivot+1, end, lt, index-(subSize+1));
+      return linearOrderStat(pivot+1, end, lt, index-(subSize));
     }
   }
 }
